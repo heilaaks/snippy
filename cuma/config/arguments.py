2cfec22cdbd9fbada7380ff35e06a3b81fa91279
Heikki Laaksonen <laaksonen.heikki.j@gmail.com>
2017-07-26 12:13:39 +0300

    Added document tags in core based on pylint
    Signed-off-by: Heikki Laaksonen <laaksonen.heikki.j@gmail.com>

--- cuma/config/arguments.py
+++ cuma/config/arguments.py
@@ -25,24 +25,32 @@ class Arguments(object):
 
     @classmethod
     def get_snippet(cls):
+        """Return the snippet that user gave from CLI exactly as it was."""
+
         cls.logger.info('parsed argument --snippet with value "{:s}"'.format(vars(cls.args)['snippet']))
 
         return vars(cls.args)['snippet']
 
     @classmethod
     def get_tags(cls):
+        """Return the tags that user gave from CLI exactly as it was."""
+
         cls.logger.info('parsed argument --tags with value "{:s}"'.format(vars(cls.args)['tags']))
 
         return vars(cls.args)['tags']
 
     @classmethod
     def get_comment(cls):
+        """Return the comment that user gave from CLI exactly as it was."""
+
         cls.logger.info('parsed argument --comment with value "{:s}"'.format(vars(cls.args)['comment']))
 
         return vars(cls.args)['comment']
 
     @classmethod
     def get_profiler(cls):
+        """Return the profiler switch based on user input or from default."""
+
         cls.logger.info('parsed argument --profile with value "{!s}"'.format(vars(cls.args)['profiler']))
 
         return vars(cls.args)['profiler']

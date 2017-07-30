4c68c0933f41f6e3d5edf39732fa885d758cfc24
Heikki Laaksonen <laaksonen.heikki.j@gmail.com>
2017-07-30 14:19:27 +0300

    Fixed the log format to adjust longes module name
    The new sqlite3_db module name was longer than the previous
    name and the log prints were not aligned correctly.
    
    Signed-off-by: Heikki Laaksonen <laaksonen.heikki.j@gmail.com>

--- snippy/logger/logger.py
+++ snippy/logger/logger.py
@@ -10,7 +10,7 @@ class Logger(object): # pylint: disable=too-few-public-methods
 
     def __init__(self):
         self.logger = logging.getLogger('root')
-        log_format = '%(asctime)s %(process)d[%(lineno)04d] <%(levelno)s>: %(threadName)s@%(filename)-12s : %(message)s'
+        log_format = '%(asctime)s %(process)d[%(lineno)04d] <%(levelno)s>: %(threadName)s@%(filename)-13s : %(message)s'
         logging.basicConfig(level='DEBUG', format=log_format)
         self.logger = logging
 

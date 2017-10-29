f21f172cc7e8c84103e8e0e97d6a2b013429a79e
Heikki Laaksonen <laaksonen.heikki.j@gmail.com>
2017-10-29 19:47:42 +0200

    Changed supported Python versions
    Removed Python 3.3 from supported Python version list. The
    works with Python 3.3 but it has been released so long time
    ago that there is no need to keep this version.
    
    Signed-off-by: Heikki Laaksonen <laaksonen.heikki.j@gmail.com>

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: POSIX :: Linux'
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',

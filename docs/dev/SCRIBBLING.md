9a37eaf2d776791b1a4fe6a48d4e03a7a704f0da
Heikki Laaksonen <laaksonen.heikki.j@gmail.com>
2017-11-12 00:07:07 +0200

    Added workflow test for --help option
    The second test in comment does not work. The reason is
    unknown. The test requires mocking the sys.exit or its
    wrapper but this causes problems with parameters being
    None. It has likely something to to do with the own
    MyHelpAction implementation.
    
    Signed-off-by: Heikki Laaksonen <laaksonen.heikki.j@gmail.com>

--- docs/dev/SCRIBBLING.md
+++ docs/dev/SCRIBBLING.md
@@ -113,6 +113,60 @@ Random notes and scribling during development.
    $ snippy-d search --sall .
    ```
 
+   ```
+   ## Programming style guide.
+   # Commit log
+   > http://keepachangelog.com/en/1.0.0/
+   ```
+
+   ```
+   # Why this slows down and starts to consume huge amount of cpu and stalls all cases=
+   # The problem is the order of sys.argv that was after the Snippy object was created.
+   import sys
+   import unittest
+   import mock
+   import pytest
+   from snippy.snip import Snippy
+   from tests.testlib.sqlite3db_helper import Sqlite3DbHelper as Database
+   
+   
+   class TestApiFramework(unittest.TestCase):
+       """Test Snippy API framework."""
+   
+       #@pytest.mark.skip(reason="mocking sys.exit in here stalls all tests with pytest with high cpu load. Why?")
+       #@mock.patch('sys.exit')
+       #def test_resets(self, mock_exit):
+       def test_resets(self):
+           """Test Snippy reset."""
+   
+           #with mock.patch('snippy.migrate.migrate.open', mock.mock_open(), create=True) as mock_file:
+           #with self.assertRaises(SystemExit): ## Blocks all logging?
+           with mock.patch('snippy.config.arguments.sys.exit'):
+               snippy = Snippy()
+               sys.argv = ['snippy', 'search']
+               cause = snippy.run_cli()
+               print(cause)
+               print("HERE")
+               print(snippy.config.is_operation_search())
+               print(snippy.config.source.get_operation())
+               print(snippy.config.is_operation_search())
+               print(snippy.config.source.get_operation())
+               print(snippy.config)
+               print("HERE-TAIL")
+               snippy.release()
+               snippy = None
+               Database.delete_storage()
+   
+           #assert 0
+   
+       # pylint: disable=duplicate-code
+       def tearDown(self):
+           """Teardown each test."""
+   
+           Database.delete_all_contents()
+           Database.delete_storage()
+   ```
+
 ## Bling
 
 http://tjelvarolsson.com/blog/five-steps-to-add-the-bling-factor-to-your-python-package/
---
+++ tests/test_wf_console_help.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python3
+
+"""test_wf_console_help.py: Test workflows for getting help from console."""
+
+import sys
+import unittest
+import mock
+from snippy.snip import Snippy
+from snippy.config.constants import Constants as Const
+from snippy.cause.cause import Cause
+from tests.testlib.sqlite3db_helper import Sqlite3DbHelper as Database
+if not Const.PYTHON2:
+    from io import StringIO # pylint: disable=import-error
+else:
+    from StringIO import StringIO # pylint: disable=import-error
+
+
+class TestWfConsoleHelp(unittest.TestCase):
+    """Test getting help from console."""
+
+    def test_console_help(self):
+        """Test getting help from consoler."""
+
+        with mock.patch('snippy.config.arguments.argparse.ArgumentParser.exit') as mocked_exit:
+            output = ('usage: snippy [-v, --version] [-h, --help] <operation> [<options>] [-vv] [-q]',
+                      '',
+                      'positional arguments:',
+                      '    {create,search,update,delete,export,import}',
+                      '',
+                      'content category:',
+                      '    --snippet                     operate snippets (default)',
+                      '    --solution                    operate solutions',
+                      '    --all                         operate all content (search only)',
+                      '',
+                      'edit options:',
+                      '    -e, --editor                  use vi editor to add content',
+                      '    -c, --content CONTENT         define example content',
+                      '    -b, --brief BRIEF             define content brief description',
+                      '    -g, --group GROUP             define content group',
+                      '    -t, --tags [TAG,...]          define comma separated list of tags',
+                      '    -l, --links [LINK ...]        define space separated list of links',
+                      '    -d, --digest DIGEST           idenfity content with digest',
+                      '',
+                      'search options:',
+                      '    --sall [KW,...]               search keywords from all fields',
+                      '    --stag [KW,...]               search keywords only from tags',
+                      '    --sgrp [KW,...]               search keywords only from groups',
+                      '    --filter REGEXP               filter search output with regexp',
+                      '    --no-ansi                     remove ANSI characters from output',
+                      '',
+                      'migration options:',
+                      '    -f, --file FILE               define file for operation',
+                      '    --defaults                    migrate category specific defaults',
+                      '    --template                    migrate category specific template',
+                      '',
+                      'symbols:',
+                      '    $    snippet',
+                      '    :    solution',
+                      '    @    group',
+                      '    #    tag',
+                      '    >    url',
+                      '',
+                      'examples:',
+                      '    Import default content.',
+                      '      $ snippy import --snippet --defaults',
+                      '      $ snippy import --solution --defaults',
+                      '',
+                      '    List all snippets.',
+                      '      $ snippy search --snippet --sall .',
+                      '',
+                      '    List more examples.',
+                      '      $ snippy --help examples',
+                      '',
+                      'Snippy version 0.6.0 - license Apache 2.0',
+                      'Copyright 2017 Heikki Laaksonen <laaksonen.heikki.j@gmail.com>',
+                      'Homepage https://github.com/heilaaks/snippy',
+                      'OK')
+            sys.argv = ['snippy', '--help']
+            real_stdout = sys.stdout
+            sys.stdout = StringIO()
+            snippy = Snippy()
+            cause = snippy.run_cli()
+            result = sys.stdout.getvalue().strip()
+            sys.stdout = real_stdout
+            assert cause == Cause.ALL_OK
+            assert mocked_exit.call_count == 2
+            assert result == Const.NEWLINE.join(output)
+            snippy.release()
+            snippy = None
+            Database.delete_storage()
+
+#    #@mock.patch('sys.exit')
+#    def test_console_help_examples(self, mocked_exit):
+#        """Test getting help from consoler."""
+#
+#        #with mock.patch('snippy.config.arguments.argparse.ArgumentParser.exit') as mocked_exit:
+#        #with mock.patch('snippy.config.arguments.sys.exit') as mocked_exit:
+#        with mock.patch('snippy.config.arguments.argparse.ArgumentParser.exit') as mocked_exit:
+#        #with self.assertRaises(SystemExit):
+#            output = ('    Creating new content:',
+#                      '      $ snippy create --snippet --editor',
+#                      '      $ snippy create --snippet -c \'docker ps\' -b \'list containers\' -t docker,moby',
+#                      '',
+#                      '    Searching and filtering content:',
+#                      '      $ snippy search --snippet --sall docker,moby',
+#                      '      $ snippy search --snippet --sall .',
+#                      '      $ snippy search --snippet --sall . --no-ansi | grep \'\$\' | sort',
+#                      '      $ snippy search --solution --sall .',
+#                      '      $ snippy search --solution --sall . | grep -Ev \'[^\s]+:\'',
+#                      '      $ snippy search --all --sall . --filter \'.*(\$\s.*)\'',
+#                      '      $ snippy search --all --sall . --no-ansi | grep -E \'[0-9]+\.\s\'',
+#                      '',
+#                      '    Updating content:',
+#                      '      $ snippy update --snippet -d 44afdd0c59e17159',
+#                      '      $ snippy update --snippet -c \'docker ps\'',
+#                      '',
+#                      '    Deleting content:',
+#                      '      $ snippy delete --snippet -d 44afdd0c59e17159',
+#                      '      $ snippy delete --snippet -c \'docker ps\'',
+#                      '',
+#                      '    Migrating default content:',
+#                      '      $ snippy import --snippet --defaults',
+#                      '      $ snippy import --solution --defaults',
+#                      '',
+#                      '    Migrating content templates:',
+#                      '      $ snippy export --solution --template',
+#                      '      $ snippy import --solution --template',
+#                      '      $ snippy import --solution -f solution-template.txt',
+#                      '',
+#                      '    Migrating specific content:',
+#                      '      $ snippy export -d e95e9092c92e3440',
+#                      '      $ snippy import -d e95e9092c92e3440 -f howto-debug-elastic-beats.txt',
+#                      '',
+#                      '    Migrating content:',
+#                      '      $ snippy export --snippet -f snippets.yaml',
+#                      '      $ snippy export --snippet -f snippets.json',
+#                      '      $ snippy export --snippet -f snippets.text',
+#                      '      $ snippy import --snippet -f snippets.yaml',
+#                      '      $ snippy export --solution -f solutions.yaml',
+#                      '      $ snippy import --solution -f solutions.yaml',
+#                      '',
+#                      'Snippy version 0.6.0 - license Apache 2.0',
+#                      'Copyright 2017 Heikki Laaksonen <laaksonen.heikki.j@gmail.com>',
+#                      'Homepage https://github.com/heilaaks/snippy')
+#            sys.argv = ['snippy', '--help', 'examples']
+#            real_stdout = sys.stdout
+#            #sys.stdout = StringIO()
+#            snippy = Snippy()
+#            cause = snippy.run_cli()
+#            result = sys.stdout.getvalue().strip()
+#            sys.stdout = real_stdout
+#            assert cause == Cause.ALL_OK
+#            assert mocked_exit.call_count == 2
+#            assert result == Const.NEWLINE.join(output)
+#            print(result)
+#            snippy.release()
+#            snippy = None
+#            Database.delete_storage()
+#        assert 0
+
+    # pylint: disable=duplicate-code
+    def tearDown(self):
+        """Teardown each test."""
+
+        Database.delete_all_contents()
+        Database.delete_storage()

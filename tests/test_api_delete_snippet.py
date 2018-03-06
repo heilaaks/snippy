a8e8341be279d8e3840c2adc1b3becb79d2f20cb
Heikki Laaksonen <laaksonen.heikki.j@gmail.com>
2018-03-06 21:42:49 +0200

    Change tests to use Pytest fixtures
    Signed-off-by: Heikki Laaksonen <laaksonen.heikki.j@gmail.com>

--- tests/test_api_delete_snippet.py
+++ tests/test_api_delete_snippet.py
@@ -17,23 +17,19 @@
 #  You should have received a copy of the GNU Affero General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""test_api_delete_snippets: Test DELETE /snippets API."""
+"""test_api_delete_snippets: Test DELETE snippets API."""
 
 from falcon import testing
 import falcon
-import mock
 import pytest
 
-from snippy.cause import Cause
-from snippy.config.config import Config
-from snippy.snip import Snippy
 from tests.testlib.content import Content
 from tests.testlib.snippet_helper import SnippetHelper as Snippet
 from tests.testlib.sqlite3db_helper import Sqlite3DbHelper as Database
 
 
 class TestApiDeleteSnippet(object):
-    """Test DELETE /snippets API."""
+    """Test DELETE snippets API."""
 
     @pytest.mark.usefixtures('server', 'snippy', 'default-snippets', 'netcat')
     def test_api_delete_snippet_001(self, snippy, mocker):
@@ -57,63 +53,55 @@ class TestApiDeleteSnippet(object):
         assert len(Database.get_snippets()) == 2
         Content.verified(mocker, snippy, content_read)
 
-    @mock.patch('snippy.server.server.SnippyServer')
-    @mock.patch('snippy.migrate.migrate.os.path.isfile')
-    @mock.patch.object(Cause, '_caller')
-    @mock.patch.object(Config, 'get_utc_time')
-    @mock.patch.object(Config, '_storage_file')
-    def test_api_delete_snippet_with_digest(self, mock_get_db_location, mock_get_utc_time, mock__caller, mock_isfile, _):
+    @pytest.mark.usefixtures('server', 'snippy', 'default-snippets', 'netcat')
+    def test_api_delete_snippet_002(self, snippy, mocker):
         """Delete snippet with digest."""
-
-        mock_isfile.return_value = True
-        mock__caller.return_value = 'snippy.testing.testing:123'
-        mock_get_db_location.return_value = Database.get_storage()
 
         ## Brief: Call DELETE /snippy/api/v1/snippets/f3fd167c64b6f97e that
         ##        matches one snippet that is deleted.
-        mock_get_utc_time.side_effect = (Snippet.CREATE_REMOVE +
-                                         Snippet.CREATE_FORCED +
-                                         Snippet.CREATE_NETCAT +
-                                         Snippet.TEST_PYTHON2)
-        snippy = Snippet.add_defaults()
-        Snippet.add_one(Snippet.NETCAT, snippy)
-        headers = {}
-        snippy = Snippy(['snippy', '--server'])
-        snippy.run()
+        content_read = {
+            Snippet.REMOVE_DIGEST: Snippet.DEFAULTS[Snippet.REMOVE],
+            Snippet.FORCED_DIGEST: Snippet.DEFAULTS[Snippet.FORCED]
+        }
+        result_headers = {}
+        snippy.run_server()
         assert len(Database.get_snippets()) == 3
-        result = testing.TestClient(snippy.server.api).simulate_delete(path='/snippy/api/v1/snippets/f3fd167c64b6f97e',  ## apiflow
-                                                                       headers={'accept': 'application/json'})
-        assert result.headers == headers
+        result = testing.TestClient(snippy.server.api).simulate_delete(  ## apiflow
+            path='/snippy/api/v1/snippets/f3fd167c64b6f97e',
+            headers={'accept': 'application/json'})
+        assert result.headers == result_headers
         assert result.status == falcon.HTTP_204
         assert len(Database.get_snippets()) == 2
-        snippy.release()
-        snippy = None
-        Database.delete_storage()
+        Content.verified(mocker, snippy, content_read)
+
+    @pytest.mark.usefixtures('server', 'snippy', 'default-snippets', 'netcat', 'caller')
+    def test_api_delete_snippet_003(self, snippy):
+        """Delete snippet with digest."""
 
         ## Brief: Try to DELETE snippet with resource location that does not
         ##        exist.
-        mock_get_utc_time.side_effect = (Snippet.CREATE_REMOVE +
-                                         Snippet.CREATE_FORCED +
-                                         Snippet.CREATE_NETCAT +
-                                         Snippet.TEST_PYTHON2)
-        snippy = Snippet.add_defaults()
-        Snippet.add_one(Snippet.NETCAT, snippy)
-        headers = {'content-type': 'application/vnd.api+json; charset=UTF-8', 'content-length': '362'}
-        body = {'meta': Snippet.get_http_metadata(),
-                'errors': [{'status': '404', 'statusString': '404 Not Found', 'module': 'snippy.testing.testing:123',
-                            'title': 'cannot find content with message digest beefbeef'}]}
-        snippy = Snippy(['snippy', '--server'])
-        snippy.run()
+        result_headers = {
+            'content-type': 'application/vnd.api+json; charset=UTF-8',
+            'content-length': '362'
+        }
+        result_json = {
+            'meta': Content.get_api_meta(),
+            'errors': [{
+                'status': '404',
+                'statusString': '404 Not Found',
+                'module': 'snippy.testing.testing:123',
+                'title': 'cannot find content with message digest beefbeef'
+            }]
+        }
+        snippy.run_server()
         assert len(Database.get_snippets()) == 3
-        result = testing.TestClient(snippy.server.api).simulate_delete(path='/snippy/api/v1/snippets/beefbeef',  ## apiflow
-                                                                       headers={'accept': 'application/json'})
-        assert result.headers == headers
-        assert Snippet.sorted_json(result.json) == Snippet.sorted_json(body)
+        result = testing.TestClient(snippy.server.api).simulate_delete(  ## apiflow
+            path='/snippy/api/v1/snippets/beefbeef',
+            headers={'accept': 'application/json'})
+        assert result.headers == result_headers
+        assert Content.ordered(result.json) == Content.ordered(result_json)
         assert result.status == falcon.HTTP_404
         assert len(Database.get_snippets()) == 3
-        snippy.release()
-        snippy = None
-        Database.delete_storage()
 
     @classmethod
     def teardown_class(cls):
--- tests/test_api_delete_solution.py
+++ tests/test_api_delete_solution.py
@@ -17,26 +17,22 @@
 #  You should have received a copy of the GNU Affero General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""test_api_delete_solutions: Test DELETE /solutions API."""
+"""test_api_delete_solutions: Test DELETE solutions API."""
 
 from falcon import testing
 import falcon
-import mock
 import pytest
 
-from snippy.cause import Cause
-from snippy.config.config import Config
-from snippy.snip import Snippy
 from tests.testlib.content import Content
 from tests.testlib.solution_helper import SolutionHelper as Solution
 from tests.testlib.sqlite3db_helper import Sqlite3DbHelper as Database
 
 
 class TestApiDeleteSolution(object):
-    """Test DELETE /solutions API."""
+    """Test DELETE solutions API."""
 
     @pytest.mark.usefixtures('server', 'snippy', 'default-solutions', 'kafka')
-    def test_api_delete_snippet_001(self, snippy, mocker):
+    def test_api_delete_solution_001(self, snippy, mocker):
         """Delete solution with DELETE."""
 
         ## Brief: Call DELETE /snippy/api/v1/solutions with digest parameter
@@ -57,63 +53,55 @@ class TestApiDeleteSolution(object):
         assert len(Database.get_solutions()) == 2
         Content.verified(mocker, snippy, content_read)
 
-    @mock.patch('snippy.server.server.SnippyServer')
-    @mock.patch('snippy.migrate.migrate.os.path.isfile')
-    @mock.patch.object(Cause, '_caller')
-    @mock.patch.object(Config, 'get_utc_time')
-    @mock.patch.object(Config, '_storage_file')
-    def test_api_delete_solution_with_digest(self, mock_get_db_location, mock_get_utc_time, mock__caller, mock_isfile, _):
+    @pytest.mark.usefixtures('server', 'snippy', 'default-solutions', 'kafka')
+    def test_api_delete_solution_002(self, snippy, mocker):
         """Delete solution with digest."""
-
-        mock_isfile.return_value = True
-        mock__caller.return_value = 'snippy.testing.testing:123'
-        mock_get_db_location.return_value = Database.get_storage()
 
         ## Brief: Call DELETE /snippy/api/v1/solutions/f3fd167c64b6f97e that
         ##        matches one solution that is deleted.
-        mock_get_utc_time.side_effect = (Solution.CREATE_BEATS_DEF +
-                                         Solution.CREATE_NGINX_DEF +
-                                         Solution.CREATE_KAFKA_DEF +
-                                         Solution.TEST_PYTHON2)
-        snippy = Solution.add_defaults()
-        Solution.add_one(Solution.KAFKA, snippy)
-        headers = {}
-        snippy = Snippy(['snippy', '--server'])
-        snippy.run()
+        content_read = {
+            Solution.BEATS_DIGEST: Solution.DEFAULTS[Solution.BEATS],
+            Solution.NGINX_DIGEST: Solution.DEFAULTS[Solution.NGINX]
+        }
+        result_headers = {}
+        snippy.run_server()
         assert len(Database.get_solutions()) == 3
-        result = testing.TestClient(snippy.server.api).simulate_delete(path='/snippy/api/v1/solutions/eeef5ca3ec9cd36',  ## apiflow
-                                                                       headers={'accept': 'application/json'})
-        assert result.headers == headers
+        result = testing.TestClient(snippy.server.api).simulate_delete(  ## apiflow
+            path='/snippy/api/v1/solutions/eeef5ca3ec9cd36',
+            headers={'accept': 'application/json'})
+        assert result.headers == result_headers
         assert result.status == falcon.HTTP_204
         assert len(Database.get_solutions()) == 2
-        snippy.release()
-        snippy = None
-        Database.delete_storage()
+        Content.verified(mocker, snippy, content_read)
+
+    @pytest.mark.usefixtures('server', 'snippy', 'default-solutions', 'kafka', 'caller')
+    def test_api_delete_solution_003(self, snippy):
+        """Delete solution with digest."""
 
         ## Brief: Try to DELETE solution with resource location that does
         ##        not exist.
-        mock_get_utc_time.side_effect = (Solution.CREATE_BEATS_DEF +
-                                         Solution.CREATE_NGINX_DEF +
-                                         Solution.CREATE_KAFKA_DEF +
-                                         Solution.TEST_PYTHON2)
-        snippy = Solution.add_defaults()
-        Solution.add_one(Solution.KAFKA, snippy)
-        headers = {'content-type': 'application/vnd.api+json; charset=UTF-8', 'content-length': '362'}
-        body = {'meta': Solution.get_http_metadata(),
-                'errors': [{'status': '404', 'statusString': '404 Not Found', 'module': 'snippy.testing.testing:123',
-                            'title': 'cannot find content with message digest beefbeef'}]}
-        snippy = Snippy(['snippy', '--server'])
-        snippy.run()
+        result_headers = {
+            'content-type': 'application/vnd.api+json; charset=UTF-8',
+            'content-length': '362'
+        }
+        result_json = {
+            'meta': Solution.get_http_metadata(),
+            'errors': [{
+                'status': '404',
+                'statusString': '404 Not Found',
+                'module': 'snippy.testing.testing:123',
+                'title': 'cannot find content with message digest beefbeef'
+            }]
+        }
+        snippy.run_server()
         assert len(Database.get_solutions()) == 3
-        result = testing.TestClient(snippy.server.api).simulate_delete(path='/snippy/api/v1/solutions/beefbeef',  ## apiflow
-                                                                       headers={'accept': 'application/json'})
-        assert result.headers == headers
-        assert Solution.sorted_json_list(result.json) == Solution.sorted_json_list(body)
+        result = testing.TestClient(snippy.server.api).simulate_delete(  ## apiflow
+            path='/snippy/api/v1/solutions/beefbeef',
+            headers={'accept': 'application/json'})
+        assert result.headers == result_headers
+        assert Content.ordered(result.json) == Content.ordered(result_json)
         assert result.status == falcon.HTTP_404
         assert len(Database.get_solutions()) == 3
-        snippy.release()
-        snippy = None
-        Database.delete_storage()
 
     @classmethod
     def teardown_class(cls):

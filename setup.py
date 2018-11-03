86ec3d2b8da88c2f783d6328081725ce301ac588
Heikki Laaksonen <laaksonen.heikki.j@gmail.com>
2018-11-03 18:36:29 +0200

    Edit Python module versions for development
    Upgraded Python test frameworks to latest versions.
    
    Signed-off-by: Heikki Laaksonen <laaksonen.heikki.j@gmail.com>

--- setup.py
+++ setup.py
@@ -52,11 +52,11 @@ extras_tests = (
     'pyflakes==1.6.0',
     'pylint==1.9.3 ; python_version=="2.7.*"',
     'pylint==2.1.1 ; python_version>"2.7"',
-    'pytest==3.9.1',
+    'pytest==3.9.3',
     'pytest-cov==2.6.0',
     'pytest-mock==1.10.0',
-    'pytest-xdist==1.23.2',
-    'tox==3.5.2'
+    'pytest-xdist==1.24.0',
+    'tox==3.5.3'
 )
 
 meta = {}

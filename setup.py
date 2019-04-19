41bae5030ce53628b70f4b918c1004a9958bb8b9
Heikki Laaksonen <laaksonen.heikki.j@gmail.com>
2019-04-19 15:24:46 +0300

    Edit Python module versions for development
    Updated Pytest, Pytest-mock and Tox versions. Removed The
    'codecov' extras since it was not needed. The cover is
    run with pytest-cov.
    
    Signed-off-by: Heikki Laaksonen <laaksonen.heikki.j@gmail.com>

--- setup.py
+++ setup.py
@@ -57,7 +57,6 @@ extras_server_pypy = (
     'psycopg2cffi==2.8.1'
 )
 extras_tests = (
-    'codecov==2.0.15',
     'flake8==3.7.7',
     'logging_tree==1.8',
     'mock==2.0.0',
@@ -65,11 +64,11 @@ extras_tests = (
     'pyflakes==2.1.1',
     'pylint==1.9.4 ; python_version=="2.7.*"',
     'pylint==2.3.1 ; python_version>"2.7"',
-    'pytest==4.4.0',
+    'pytest==4.4.1',
     'pytest-cov==2.6.1',
-    'pytest-mock==1.10.3',
+    'pytest-mock==1.10.4',
     'pytest-xdist==1.28.0',
-    'tox==3.8.6'
+    'tox==3.9.0'
 )
 
 meta = {}

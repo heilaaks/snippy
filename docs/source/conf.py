5b74e72cfad63bf6a6c566439cd2ece934377ff3
Heikki Laaksonen <laaksonen.heikki.j@gmail.com>
2020-03-05 03:05:10 -0600

    Edit commented Sphinx path manipulation
    For local environment and for the automated document build
    in Read the Docs there seem to be no reason to add the
    'sys.path.insert' for the Sphinx build as of now. There was
    a situation that required locally setting the path. But
    this started to work again.
    
    Previously the path was '.' which did not work and it had
    to be changed to '../../../snippy'. But since the 'make docs'
    is working locally, this is not taken into use. Just that
    the change in comments is stored for further investigation.
    
    Signed-off-by: Heikki Laaksonen <laaksonen.heikki.j@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@
 import io
 # import os
 # import sys
-# sys.path.insert(0, os.path.abspath('.'))
+# sys.path.insert(0, os.path.abspath('../../../snippy'))
 
 
 # -- Project information -----------------------------------------------------
